46dd43484c912a692dfff2b8f03039947dd5e42a
e <1414200300@qq.com>
2018-12-12 11:39:26 +0800

    Site updated: 2018-12-12 11:39:24

--- js/diaspora.js
+++ js/diaspora.js
@@ -485,3 +485,10 @@ $(function() {
     console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/Fechin/hexo-theme-diaspora")
 })
 
+
+ (function(i,s,o,g,r,a,m){i["DaoVoiceObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;a.charset="utf-8";m.parentNode.insertBefore(a,m)})(window,document,"script",('https:' == document.location.protocol ? 'https:' : 'http:') + "//widget.daovoice.io/widget/0f81ff2f.js","daovoice")
+  daovoice('init', {
+      app_id: "4acee80f"
+    });
+  daovoice('update');
+
